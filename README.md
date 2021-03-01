4fb2923dd092eeb9c8fa250b01dbf7e54b6b4d75
coder137 <coder137@gmail.com>
2021-03-01 15:57:39 -0800

    Added cpp example

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
+
+
 # Jetson_Nano
 
 Jetson_Nano 2GB
+
+# Folder Structure
+
+- cpp
+  - C++ code examples
+- gpio
+  - Test GPIO input and output via Python script
---
+++ cpp/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int, char **)
+{
+    std::cout << "hello, the world" << std::endl;
+    std::cout << "Niket Naidu, 0115" << std::endl;
+}
